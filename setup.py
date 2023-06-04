f9243616c56236e1258a47326c5a7d8aeb5e91b9
adepu.srujana <adepu.srujana99@gmail.com>
2023-06-04 15:05:02 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_dot='-e .'
+def get_requirements(file_path:str)-> List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements] 
+        if HYPHEN_E_dot in requirements:
+            requirements.remove(HYPHEN_E_dot)
+    return requirements
+setup(
+    name='MLProject',
+    version='0.0.1',
+    author='sruj',
+    author_email='adepu.srujana99@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+
+)
